cf57cc0818e2a7fda285bf8a9882c806e67c8dca
SevaStopAll <Sevastopallrh@yandex.ru>
2022-12-03 16:28:17 +0400

    1.8. Состояние объекта [#167388]

---
+++ src/main/java/ru/job4j/oop/Battery.java
@@ -0,0 +1,18 @@
+package ru.job4j.oop;
+
+public class Battery {
+    private int load;
+
+    public Battery(int load) {
+        this.load = load;
+    }
+
+    public int getLoad() {
+        return load;
+    }
+
+    public void exchange(Battery another) {
+        another.load += this.getLoad();
+        this.load = 0;
+    }
+}
---
+++ src/test/java/ru/job4j/oop/BatteryTest.java
@@ -0,0 +1,34 @@
+package ru.job4j.oop;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BatteryTest {
+
+    @Test
+    public void whenThis30Another50ThenAnotherLoadEqual80() {
+        Battery charger = new Battery(30);
+        Battery another = new Battery(50);
+        charger.exchange(another);
+        int expected = 80;
+        assertThat(another.getLoad()).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenThis80Another20ThenThisLoadEqual0() {
+        Battery charger = new Battery(80);
+        Battery another = new Battery(20);
+        charger.exchange(another);
+        int expected = 0;
+        assertThat(charger.getLoad()).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenThis95Another0ThenAnotherLoadEqual80() {
+        Battery charger = new Battery(95);
+        Battery another = new Battery(0);
+        charger.exchange(another);
+        int expected = 95;
+        assertThat(another.getLoad()).isEqualTo(expected);
+    }
+}
