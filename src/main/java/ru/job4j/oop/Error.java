6f9046410b63ff14ca2322c205469cdddc1a4f79
SevaStopAll <Sevastopallrh@yandex.ru>
2022-12-03 14:29:09 +0400

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,32 @@
+package ru.job4j.oop;
+
+public class Error {
+    boolean active;
+    int status;
+    String message;
+
+    public Error() {
+
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void printInfo() {
+        System.out.println("Is active? " + active);
+        System.out.println("Error status: " + status);
+        System.out.println("Printed message " + message);
+    }
+
+    public static void main(String[] args) {
+        Error memory = new Error(true, 65, "Out of memory");
+        Error random = new Error();
+        Error router = new Error(true, 404, "Your router is switched off");
+        memory.printInfo();
+        random.printInfo();
+        router.printInfo();
+    }
+}
