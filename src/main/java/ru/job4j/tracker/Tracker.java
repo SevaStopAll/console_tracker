745f645cf43dc87d0a33ee14982bce885eded991
Sevastopall <Sevastopallrh@yandex.ru>
2022-12-05 13:03:11 +0400

    7. Метод удаления заявки Tracker.delete [#211749 #367563]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -60,7 +60,7 @@ public class Tracker {
         int index = indexOf(id);
         boolean result = index != -1;
         if (result) {
-            System.arraycopy(items, id + 1, items, id, size - index - 1);
+            System.arraycopy(items, index + 1, items, index, size - index - 1);
             items[size - 1] = null;
             size--;
         }
