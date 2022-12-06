d543b9d0857c5e5f706396d05c575768eed1dfe1
SevaStopAll <Sevastopallrh@yandex.ru>
2022-12-06 14:34:44 +0400

    7. Шаблон проектирования - Стратегия [#786]

---
+++ src/main/java/ru/job4j/strategy/Paint.java
@@ -0,0 +1,14 @@
+package ru.job4j.strategy;
+
+public class Paint {
+
+    public void draw(Shape shape) {
+        System.out.print(shape.draw());
+    }
+
+    public static void main(String[] args) {
+        Paint paint = new Paint();
+        paint.draw(new Triangle());
+        paint.draw(new Square());
+    }
+}
