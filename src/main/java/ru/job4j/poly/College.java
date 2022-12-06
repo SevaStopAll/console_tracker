4d623dcae9808ee5e28b319403b55227c46d9d8a
SevaStopAll <Sevastopallrh@yandex.ru>
2022-12-06 20:08:42 +0400

    3.1. Приведение типов. Повышение типа и понижение типа. [#181322 #368082]

--- src/main/java/ru/job4j/poly/College.java
+++ src/main/java/ru/job4j/poly/College.java
@@ -2,9 +2,9 @@ package ru.job4j.poly;
 
 public class College {
     public static void main(String[] args) {
-    Freshman alex = new Freshman();
-    Student barry = alex;
-    Object human = alex;
+        Freshman alex = new Freshman();
+        Student barry = alex;
+        Object human = alex;
     }
 
 }
