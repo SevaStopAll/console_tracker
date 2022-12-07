6a848fb8d68efad843092862f3eef36bb1ac7b66
SevaStopAll <Sevastopallrh@yandex.ru>
2022-12-07 21:19:25 +0400

    0.4. Пользовательские исключения. [#219367]

---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,22 @@
+package ru.job4j.ex;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        for (int index = 0; index < value.length; index++) {
+            if (value[index].equals(key)) {
+                return index;
+            }
+        }
+        throw new ElementNotFoundException("Element wasn't found");
+    }
+
+    public static void main(String[] args) {
+        String[] value = {"Not a Key", "Not a key", "Not a key"};
+        String key = "Key";
+        try {
+            System.out.println(indexOf(value, key));
+        } catch (ElementNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
