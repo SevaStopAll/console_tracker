d7b9ec3525fcb543385510da8fa649fbb40eeb32
Sevastopall <Sevastopallrh@yandex.ru>
2022-12-07 13:09:50 +0400

    0.4. Пользовательские исключения. [#219367]

---
+++ src/main/java/ru/job4j/ex/ElementNotFoundException.java
@@ -0,0 +1,7 @@
+package ru.job4j.ex;
+
+public class ElementNotFoundException extends java.lang.Exception {
+    public ElementNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,13 @@
+package ru.job4j.ex;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int rsl = -1;
+        for (int index = 0; index < value.length; index++) {
+            if (value[index].equals(key)) {
+                rsl = index;
+            }
+        }
+        return rsl;
+    }
+}
