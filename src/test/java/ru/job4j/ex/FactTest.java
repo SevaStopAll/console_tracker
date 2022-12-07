18e142be393686b69cc4842dc9b3a9bfcc022ba6
SevaStopAll <Sevastopallrh@yandex.ru>
2022-12-07 21:09:44 +0400

    0.3. Тестирование исключений с junit [#219366 #368466]

--- src/test/java/ru/job4j/ex/FactTest.java
+++ src/test/java/ru/job4j/ex/FactTest.java
@@ -7,13 +7,21 @@ import static org.junit.Assert.assertThrows;
 class FactTest {
 
   @Test
-    public void whenException() {
-      IllegalAccessException exception = assertThrows(
-              IllegalAccessException.class,
-              ()    -> {
-                  Fact.calc(-1);
-              });
-      assertThat(exception.getMessage()).isEqualTo("N could not be less then 0.");
-              }
+public void whenException() {
+    IllegalAccessException exception = assertThrows(
+        IllegalAccessException.class,
+        ()    -> {
+        Fact.calc(-1);
+        });
+    assertThat(exception.getMessage()).isEqualTo("N could not be less then 0.");
+}
+
+    @Test
+public void when3Then6() throws IllegalAccessException {
+    int number = 3;
+    int expected = 6;
+    int result = Fact.calc(number);
+    assertThat(result).isEqualTo(expected);
+}
 
   }
