2b53ad392015f58ca84c5738082e2e438cbc803b
SevaStopAll <Sevastopallrh@yandex.ru>
2022-12-09 17:38:23 +0400

    4. Принципы раннего возврата и охранных выражений [#504875]

--- src/main/java/ru/job4j/early/PasswordValidator.java
+++ src/main/java/ru/job4j/early/PasswordValidator.java
@@ -3,32 +3,39 @@ package ru.job4j.early;
 public class PasswordValidator {
 
     public static String validate(String password) {
+        String[] denied = {"qwerty", "12345", "password", "user", "admin"};
         if (password == null) {
             throw new IllegalArgumentException("Password can't be null");
         }
         if (password.length() < 8 || password.length() > 32) {
             throw new IllegalArgumentException("Password should be length [8, 32]");
         }
-        if (password.toLowerCase().contains("qwerty") || password.toLowerCase().contains("12345") || password.toLowerCase().contains("password")
-                || password.toLowerCase().contains("user") || password.toLowerCase().contains("admin")) {
-            throw new IllegalArgumentException("Password shouldn't contain substrings: qwerty, 12345, password, admin, user");
+        for (String option : denied) {
+            if (password.toLowerCase().contains(option)) {
+                throw new IllegalArgumentException("Password shouldn't contain substrings: qwerty, 12345, password, admin, user");
+            }
         }
-        int upper = 0;
-        int lower = 0;
-        int digits = 0;
-        int special = 0;
-        for (int i = 0; i < password.length(); i++) {
-            if (Character.isDigit(password.charAt(i))) {
+        return checkChars(password);
+    }
+
+    public static String checkChars(String password) {
+        int upper = 0, lower = 0, digits = 0, special = 0;
+        char[] symbols = password.toCharArray();
+        for (char symbol : symbols) {
+            if (Character.isDigit(symbol)) {
                 digits++;
             }
-            if (Character.isLowerCase(password.charAt(i))) {
+            if (Character.isLowerCase(symbol)) {
                 lower++;
             }
-            if (Character.isUpperCase(password.charAt(i))) {
+            if (Character.isUpperCase(symbol)) {
                 upper++;
             }
-            if (!Character.isLetterOrDigit(password.charAt(i))) {
+            if (!Character.isLetterOrDigit(symbol)) {
                 special++;
+            }
+            if (upper > 0 && lower > 0 && digits > 0 && special > 0) {
+                break;
             }
         }
         if (upper == 0) {
@@ -37,9 +44,9 @@ public class PasswordValidator {
         if (lower == 0) {
             throw new IllegalArgumentException("Password should contain at least one lowercase letter");
         }
-       if (digits == 0) {
-           throw new IllegalArgumentException("Password should contain at least one figure");
-       }
+        if (digits == 0) {
+            throw new IllegalArgumentException("Password should contain at least one figure");
+        }
         if (special == 0) {
             throw new IllegalArgumentException("Password should contain at least one special symbol");
         }
