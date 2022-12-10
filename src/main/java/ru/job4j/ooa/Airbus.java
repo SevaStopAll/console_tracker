a3ddc323d40c87f7ce9eb79946cd5f9492851c1a
SevaStopAll <Sevastopallrh@yandex.ru>
2022-12-10 12:00:35 +0400

    3. final [#334073]

---
+++ src/main/java/ru/job4j/ooa/Airbus.java
@@ -0,0 +1,35 @@
+package ru.job4j.ooa;
+
+public final class Airbus extends Aircraft {
+    private static final int COUNT_ENGINE = 2;
+    private String name;
+
+    public Airbus(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void printModel() {
+        System.out.println("Модель самолета: " + name);
+    }
+
+    public void printCountEngine() {
+        int engines = "A380".equals(name) ? COUNT_ENGINE : 4;
+        System.out.println("Количество двигателей равно: " + engines);
+    }
+
+    @Override
+    public String toString() {
+        return "Airbus{"
+                + "name='" + name + '\''
+                + '}';
+    }
+}
