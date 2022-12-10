48f40be76bf734a2f4f2df189fbeb14d3206b6dd
SevaStopAll <Sevastopallrh@yandex.ru>
2022-12-10 19:46:12 +0400

    1. Телефонный справочник на базе ArrayList [#41598]

---
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -0,0 +1,21 @@
+package ru.job4j.search;
+
+import java.util.ArrayList;
+
+public class PhoneDictionary {
+    private ArrayList<Person> persons = new ArrayList<>();
+
+    public void add(Person person) {
+        this.persons.add(person);
+    }
+
+    public ArrayList<Person> find(String key) {
+        ArrayList<Person> result = new ArrayList<>();
+        for (Person person : persons) {
+            if (key.equals(person.getPhone()) || key.equals(person.getName()) || key.equals(person.getAddress()) || key.equals(person.getSurname())) {
+                result.add(person);
+            }
+        }
+        return result;
+    }
+}
---
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -0,0 +1,35 @@
+package ru.job4j.search;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertThrows;
+
+public class PhoneDictionaryTest {
+    @Test
+    public void whenFindByName() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        ArrayList<Person> persons = phones.find("Petr");
+        assertThat(persons.get(0).getSurname()).isEqualTo("Arsentev");
+    }
+
+    @Test
+    public void whenOutOfBoundsException() {
+        IndexOutOfBoundsException exception = assertThrows(
+                IndexOutOfBoundsException.class,
+                () -> {
+                    PhoneDictionary phones = new PhoneDictionary();
+                    phones.add(
+                            new Person("Petr", "Arsentev", "534872", "Bryansk")
+                    );
+                    ArrayList<Person> persons = phones.find("Kirill");
+                    persons.get(0).getSurname();
+                });
+        assertThat(exception.getMessage()).isEqualTo("Index 0 out of bounds for length 0");
+    }
+}
