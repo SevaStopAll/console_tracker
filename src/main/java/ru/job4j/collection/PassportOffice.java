32d107ef3c70acd26d750e347e7bfc4fcb82829f
SevaStopAll <Sevastopallrh@yandex.ru>
2022-12-11 14:45:07 +0400

    1. Паспорт и жители [#212656 #369905]

--- src/main/java/ru/job4j/collection/PassportOffice.java
+++ src/main/java/ru/job4j/collection/PassportOffice.java
@@ -10,11 +10,13 @@ public class PassportOffice {
         boolean rsl = false;
         if (!citizens.containsKey(citizen.getPassport())) {
             citizens.put(citizen.getPassport(), citizen);
+            rsl = true;
         }
         return rsl;
     }
 
     public Citizen get(String passport) {
+
         return citizens.get(passport);
     }
 }
