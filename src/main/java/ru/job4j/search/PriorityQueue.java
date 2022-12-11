44b93369a313564ea1136face436e96180d3ff4a
SevaStopAll <Sevastopallrh@yandex.ru>
2022-12-11 07:39:37 +0400

    2. Очередь с приоритетом на LinkedList [#41670 #369793]

--- src/main/java/ru/job4j/search/PriorityQueue.java
+++ src/main/java/ru/job4j/search/PriorityQueue.java
@@ -8,9 +8,10 @@ public class PriorityQueue {
     public void put(Task task) {
         int index = 0;
         for (Task element : tasks) {
-            if (element.getPriority() <= task.getPriority()) {
-                index++;
+            if (element.getPriority() > task.getPriority()) {
+                break;
             }
+            index++;
         }
         this.tasks.add(index, task);
     }
