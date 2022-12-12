e341224bff57cd90a5ce61456d9cca9a21bb6655
SevaStopAll <Sevastopallrh@yandex.ru>
2022-12-12 14:45:00 +0400

    2. Комбинированный компаратор [#10036]

--- src/main/java/ru/job4j/collection/JobSorter.java
+++ src/main/java/ru/job4j/collection/JobSorter.java
@@ -6,12 +6,12 @@ import java.util.List;
 public class JobSorter {
     public static void main(String[] args) {
         List<Job> jobs = Arrays.asList(
-                new Job("Fix bugs", 4),
-                new Job("Impl task", 2),
-                new Job("Reboot server", 1)
+                new Job("Fix bug", 1),
+                new Job("Fix bug", 4),
+                new Job("Fix bug", 2),
+                new Job("X task", 0)
         );
-        System.out.println(jobs);
-        jobs.sort(new SortDescByNameJob());
+        jobs.sort(new JobDescByName().thenComparing(new JobDescByPriority()));
         System.out.println(jobs);
     }
 }
