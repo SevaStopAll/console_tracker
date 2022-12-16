66cc6e37a2064abdf5ef0a7d8e880acb009ff9c4
SevaStopAll <Sevastopallrh@yandex.ru>
2022-12-16 20:36:30 +0400

    2. Отсортировать департаменты [#34352 #371582]

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -5,9 +5,7 @@ import java.util.Comparator;
 public class DepDescComp implements Comparator<String> {
     @Override
     public int compare(String o1, String o2) {
-        if (!o2.split("/")[0].equals(o1.split("/")[0])) {
-            return o2.split("/")[0].compareTo(o1.split("/")[0]);
-        }
-        return o1.compareTo(o2);
+        int result = o2.split("/")[0].compareTo(o1.split("/")[0]);
+        return result != 0 ? result : o1.compareTo(o2);
     }
 }
