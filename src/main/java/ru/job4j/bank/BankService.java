65deaf00c0a395450e4e042872ea3558a2b7aa51
Sevastopall <Sevastopallrh@yandex.ru>
2022-12-19 10:05:58 +0400

    17. Тестовое задание из модуля коллекции Lite переделать на Stream API. [#24260]

--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Stream;
 
 /**
  * Класс позволяет проводить банковские переводы между счетами, рабоатать с базой клиентов и счетов,
