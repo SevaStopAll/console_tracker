c24b27acacd2fa83a56c5fa385fbaacccb367f47
SevaStopAll <Sevastopallrh@yandex.ru>
2023-02-03 22:29:23 +0400

    2. Трекер SQL [#1734] I realize all methods but now i want to watch my code tomorrow to check its style.

--- src/main/java/ru/job4j/tracker/store/SqlTracker.java
+++ src/main/java/ru/job4j/tracker/store/SqlTracker.java
@@ -5,7 +5,6 @@ import ru.job4j.tracker.Store;
 
 import java.io.InputStream;
 import java.sql.*;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
@@ -53,31 +52,49 @@ public class SqlTracker implements Store {
             Timestamp timestamp = new Timestamp(millis);
             ps.setTimestamp(2, timestamp);
             ps.execute();
-        }  catch (SQLException ex) {
-                throw new RuntimeException(ex);
-            }
+        } catch (SQLException ex) {
+            throw new RuntimeException(ex);
+        }
         return item;
     }
 
     @Override
     public boolean replace(int id, Item item) {
-        return false;
+        boolean result = false;
+        if (findById(id) != null) {
+            try (PreparedStatement ps = cn.prepareStatement("update items set name = ? where id = ?")) {
+                ps.setString(1, item.getName());
+                ps.setInt(2, id);
+                result = ps.executeUpdate() > 0;
+            } catch (SQLException ex) {
+                throw new RuntimeException(ex);
+            }
+        }
+        return result;
     }
 
     @Override
     public boolean delete(int id) {
-        return false;
+        boolean result = false;
+            try (PreparedStatement ps = cn.prepareStatement("delete from items where id = ?")) {
+                ps.setInt(1, id);
+                 result = ps.executeUpdate() > 0;
+            } catch (SQLException ex) {
+                throw new RuntimeException(ex);
+            }
+
+        return result;
     }
 
     @Override
-    public List<Item> findAll(){
+    public List<Item> findAll() {
         List<Item> items = new ArrayList<>();
         try (Statement statement = cn.createStatement()) {
-            ResultSet result = statement.executeQuery("select * from items;");
-            while (result.next()) {
-                items.add(new Item(result.getString("name"),
-                        result.getInt("id"),
-                        result.getTimestamp("created").toLocalDateTime()));
+            ResultSet resultSet = statement.executeQuery("select * from items;");
+            while (resultSet.next()) {
+                items.add(new Item(resultSet.getString("name"),
+                        resultSet.getInt("id"),
+                        resultSet.getTimestamp("created").toLocalDateTime()));
             }
         } catch (SQLException e) {
             throw new RuntimeException(e);
@@ -87,30 +104,36 @@ public class SqlTracker implements Store {
 
     @Override
     public List<Item> findByName(String key) {
-/*        try (PreparedStatement ps = cn.prepareStatement("insert into users(name, data) values (?, ?)")) {
-            ps.setString(1, item.getName());
-            long millis = System.currentTimeMillis();
-            Timestamp timestamp = new Timestamp(millis);
-            ps.setTimestamp(2, timestamp);
-            ps.execute();
-        }  catch (SQLException ex) {
-            throw new RuntimeException(ex);
-        }*/
-        return null;
+        List<Item> items = new ArrayList<>();
+        try (PreparedStatement statement = cn.prepareStatement("select * from items where name = ?")) {
+            statement.setString(1, key);
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                items.add(new Item(resultSet.getString("name"),
+                        resultSet.getInt("id"),
+                        resultSet.getTimestamp("created").toLocalDateTime()));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return items;
     }
 
     @Override
     public Item findById(int id) {
- /*       try (PreparedStatement ps = cn.prepareStatement("insert into users(name, data) values (?, ?)")) {
-            ps.setString(1, item.getName());
-            long millis = System.currentTimeMillis();
-            Timestamp timestamp = new Timestamp(millis);
-            ps.setTimestamp(2, timestamp);
-            ps.execute();
-        }  catch (SQLException ex) {
-            throw new RuntimeException(ex);
-        }*/
-        return null;
+        Item result = null;
+        try (PreparedStatement statement = cn.prepareStatement("select * from items where id = ?")) {
+            statement.setInt(1, id);
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                result = new Item(resultSet.getString("name"),
+                        resultSet.getInt("id"),
+                        resultSet.getTimestamp("created").toLocalDateTime());
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return result;
     }
 }
 
