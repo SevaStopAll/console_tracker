cc7849407c9b878e3311f4353019b187313c30c8
SevaStopAll <Sevastopallrh@yandex.ru>
2023-02-05 21:04:21 +0400

    2. Трекер SQL [#1734 #388105] Refactor my code and try to pass CI

--- src/main/java/ru/job4j/tracker/store/SqlTracker.java
+++ src/main/java/ru/job4j/tracker/store/SqlTracker.java
@@ -44,16 +44,28 @@ public class SqlTracker implements Store {
         }
     }
 
+    private Item createItem(ResultSet resultSet) throws SQLException {
+        Item item = null;
+             item = new Item(resultSet.getString("name"),
+                    resultSet.getInt("id"),
+                    resultSet.getTimestamp("created").toLocalDateTime());
+        return item;
+    }
     @Override
     public Item add(Item item) {
-        try (PreparedStatement ps = cn.prepareStatement("insert into items(name, created) values (?, ?)")) {
+        try (PreparedStatement ps =
+                     cn.prepareStatement("insert into items(name, created) values (?, ?)",
+                Statement.RETURN_GENERATED_KEYS)) {
             ps.setString(1, item.getName());
-            long millis = System.currentTimeMillis();
-            Timestamp timestamp = new Timestamp(millis);
-            ps.setTimestamp(2, timestamp);
+            ps.setTimestamp(2, Timestamp.valueOf(item.getCreated()));
             ps.execute();
-        } catch (SQLException ex) {
-            throw new RuntimeException(ex);
+            try (ResultSet generatedKeys = ps.getGeneratedKeys()) {
+                if (generatedKeys.next()) {
+                    item.setId(generatedKeys.getInt(1));
+                }
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
         }
         return item;
     }
@@ -61,15 +73,15 @@ public class SqlTracker implements Store {
     @Override
     public boolean replace(int id, Item item) {
         boolean result = false;
-        if (findById(id) != null) {
-            try (PreparedStatement ps = cn.prepareStatement("update items set name = ? where id = ?")) {
+            try (PreparedStatement ps =
+                         cn.prepareStatement("update items set name = ?, created = ? where id = ?")) {
                 ps.setString(1, item.getName());
-                ps.setInt(2, id);
+                ps.setTimestamp(2, Timestamp.valueOf(item.getCreated()));
+                ps.setInt(3, id);
                 result = ps.executeUpdate() > 0;
             } catch (SQLException ex) {
                 throw new RuntimeException(ex);
             }
-        }
         return result;
     }
 
@@ -92,9 +104,7 @@ public class SqlTracker implements Store {
         try (Statement statement = cn.createStatement()) {
             ResultSet resultSet = statement.executeQuery("select * from items;");
             while (resultSet.next()) {
-                items.add(new Item(resultSet.getString("name"),
-                        resultSet.getInt("id"),
-                        resultSet.getTimestamp("created").toLocalDateTime()));
+                items.add(createItem(resultSet));
             }
         } catch (SQLException e) {
             throw new RuntimeException(e);
@@ -109,9 +119,7 @@ public class SqlTracker implements Store {
             statement.setString(1, key);
             ResultSet resultSet = statement.executeQuery();
             while (resultSet.next()) {
-                items.add(new Item(resultSet.getString("name"),
-                        resultSet.getInt("id"),
-                        resultSet.getTimestamp("created").toLocalDateTime()));
+                items.add(createItem(resultSet));
             }
         } catch (SQLException e) {
             throw new RuntimeException(e);
@@ -125,11 +133,7 @@ public class SqlTracker implements Store {
         try (PreparedStatement statement = cn.prepareStatement("select * from items where id = ?")) {
             statement.setInt(1, id);
             ResultSet resultSet = statement.executeQuery();
-            if (resultSet.next()) {
-                result = new Item(resultSet.getString("name"),
-                        resultSet.getInt("id"),
-                        resultSet.getTimestamp("created").toLocalDateTime());
-            }
+            result = createItem(resultSet);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
